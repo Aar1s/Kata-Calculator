5c8f6f51391a477dc48cfe148be1a8944b99be94
Aar1s <65885326+Aar1s@users.noreply.github.com>
2022-03-20 22:29:33 +0500

    some parts are working

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] expression = scanner.nextLine().split(" ");
-        numCheck(expression[0]);
+        if (numCheck(expression[0]) != 0 && (numCheck(expression[2]) != 0) {
+
+        }
         numCheck(expression[2]);
         int result;
-
         switch (expression[1]){
             case "+":
                 result = addition(Integer.parseInt(expression[0]), Integer.parseInt(expression[2]));
@@ -51,7 +51,21 @@ public class Main {
     public static enum Roman {
         I, II, III, IV, V, VI, VII, VIII, IX, X
     }
+}
+class Parser {
+    public static Scanner scanner = new Scanner(System.in);
+     int a;
+     int b;
 
+    public static void parsing(String expression) {
+        String[] scannedExpression = scanner.nextLine().split(" ");
+        if (scannedExpression.length != 3 || !scannedExpression[1].equals("+") && !scannedExpression[1].equals("-") &&
+                !scannedExpression[1].equals("*") && !scannedExpression[1].equals("/") ) {
+            System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        } else if ((numCheck(scannedExpression[0]) > 0) && numCheck(scannedExpression[2]) > 0){
+            numCheck(scannedExpression[0]);
+        }
+    }
     public static int numCheck(String a) {
         try {
             Integer.parseInt(a);
@@ -59,18 +73,18 @@ public class Main {
             if (romanToArabic(a) != 0) {
                 return romanToArabic(a);
             }
-            System.out.println("Throws exception");
-            } return 0;
-        }
-
+            System.out.println("Throws exception т.к. не является математической операцией.");
+        } return 0;
+    }
 
     public static int romanToArabic(String a) {
         int index = 1;
-        for (Roman value : Roman.values()) {
+        for (Main.Roman value : Main.Roman.values()) {
             if (value.name().equals(a)) {
                 return index;
             }
             index++;
-            }return 0;
-        }
+        }return 0;
+    }
 }
+
